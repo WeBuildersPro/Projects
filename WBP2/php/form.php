d0324fea8dd8c30b71a82c638a994ed8240ddeb5
Unknown <webuilderspro@gmail.com>
2017-08-09 12:39:00 -0400

    php form

--- WBP2/php/form.php
+++ WBP2/php/form.php
@@ -1,9 +1,4 @@
 <?php
-if(!isset($_POST['submit']))
-{
-	//This page should not be accessed directly. Need to submit the form.
-	echo "error; you need to submit the form!";
-}
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $message = $_POST['message'];
@@ -32,7 +27,7 @@ $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
 mail($to,$email_subject,$email_body,$headers);
 // redirect to index.html
-header('Location: /index.html');
+header('Location: ../index.html');
 
 
 // Function to validate against any email injection attempts
